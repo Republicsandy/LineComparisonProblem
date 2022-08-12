2dbbbec2cc4563571728a25768d8139940d0d81f
Republicsandy <sandy180388@gmail.com>
2022-08-12 07:33:24 +0530

    [sandeep] UC_1_LengthOfLine file is addded

---
+++ LineComparisonProblem/LengthOfLine.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace LineComparisonProblem
+{
+    class UC_1_LengthOfLine
+    {
+        public void FindLengthOfLine()
+        {
+            int x1, x2, y1, y2;
+            Console.WriteLine("Enter value of x1 and y1 for line");
+            x1 = Convert.ToInt32(Console.ReadLine());
+            y1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter value of x2 and y2 for line");
+            x2 = Convert.ToInt32(Console.ReadLine());
+            y2 = Convert.ToInt32(Console.ReadLine());
+            double length = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+            Console.WriteLine("length of Line is : " + length);
+        }
+    }
+}
--- LineComparisonProblem/Program.cs
+++ LineComparisonProblem/Program.cs
@@ -8,6 +8,30 @@ namespace LineComparisonProblem
         {
             Console.WriteLine("Welcome to Line Comparison Computation");
 
+            Console.WriteLine("Choose a number to go to the respective problem");
+            Console.WriteLine("1 for find the length of line");
+            Console.WriteLine("2 for check the line is equal or not");
+            Console.WriteLine("3 for find the difference of the line");
+
+            int Option = Convert.ToInt32(Console.ReadLine());
+
+            switch (Option)
+            {
+
+                case 1:
+                    UC_1_LengthOfLine qemrem = new UC_1_LengthOfLine();
+                    qemrem.FindLengthOfLine();
+                    break;
+                case 2:
+                    UC_2_CheckEquality swapnum = new UC_2_CheckEquality();
+                    swapnum.CheckEquality();
+                    break;
+                case 3:
+                    UC_3_CompareTowLines evenodd = new UC_3_CompareTowLines();
+                    evenodd.CompareTowLines();
+                    break;
+            }
+
         }
     }
 }
