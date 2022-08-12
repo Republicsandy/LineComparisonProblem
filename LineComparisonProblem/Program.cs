7bcef82127e1bcb8ce24c7e1331469820bd438c3
Republicsandy <sandy180388@gmail.com>
2022-08-12 07:26:40 +0530

    [sandeep] Line Comprarison main file addded

---
+++ LineComparisonProblem/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace LineComparisonProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Line Comparison Computation");
+
+        }
+    }
+}
